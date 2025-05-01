c5943a753bfc394eda8915ccf348c9cdbff32bd1
JesseVolstruis <2597626@students.wits.ac.za>
2025-05-01 15:53:47 +0200

    more switch block logic

--- Assets/Scripts/SwitchManager.cs
+++ Assets/Scripts/SwitchManager.cs
@@ -6,17 +6,48 @@ public class SwitchManager : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]
-    private GameObject[] switchesActive;
+    private GameObject[] switches;
     [SerializeField]
-    private GameObject[] switchesInactive;
+    private PlayerManager playerManager;
     void Start()
     {
+        switches = GameObject.FindGameObjectsWithTag("SwitchBlock");
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isJumped())
+        {
+            ToggleSwitches();
+        }
         
+    }
+
+    bool isJumped()
+    {
+        return (playerManager.hasJump && Input.GetKeyDown(KeyCode.Space));
+    }
+
+    void ToggleSwitches()
+    {
+        foreach (GameObject s in switches)
+        {
+            SpriteRenderer sprite = s.GetComponent<SpriteRenderer>();
+            if(sprite.color.a == 1)
+            {
+                Color tmpColor = sprite.color;
+                tmpColor.a = 0.0588f;
+                sprite.color = tmpColor;
+            }
+            else
+            {
+                Color tmpColor = sprite.color;
+                tmpColor.a = 1;
+                sprite.color = tmpColor;
+            }
+        }
+        Debug.Log("And jump");
     }
 }
